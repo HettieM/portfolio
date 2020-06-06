0382d2592f0f120066e1d3637c45bc521df12945
Hettie McConnell <hettie.mcconnell@outlook.com>
2020-06-06 10:11:47 +0100

    Add tech stack and projects outline

---
+++ index.js
@@ -0,0 +1,12 @@
+// Infinite marquee for the links
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue(
+  "--marquee-elements-displayed"
+);
+const marqueeContent = document.querySelector("ul.links-logo");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
